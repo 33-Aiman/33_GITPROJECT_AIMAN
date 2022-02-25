a6d833c0f298af2ac14f0ae14cfe0a6e0a7661a3
33_AimanEzrafil <phoenexus2107@gmail.com>
2022-02-25 13:24:47 +0800

    InfiniteScript made

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -7,21 +7,42 @@ public class TileManager : MonoBehaviour
     public GameObject[] tilePrefabs;
     public float zSpawn = 0;
     public float tileLength = 30;
+    public int numberOfTiles = 5;
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    public Transform playerTransform;
+
     void Start()
     {
-        SpawnTile(0);
-        SpawnTile(1);
-        SpawnTile(4);
+        for (int i = 0; i < numberOfTiles; i++)
+        {
+            if (i == 0)
+                SpawnTile(0);
+            else
+                SpawnTile(Random.Range(0, tilePrefabs.Length));
+        }
     }
 
    
     void Update()
     {
-        
+        if (playerTransform.position.z -35 > zSpawn - (numberOfTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+            DeleteTile();
+        }
     }
     public void SpawnTile(int tileIndex)
     {
-        Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        GameObject go = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        activeTiles.Add(go);
         zSpawn += tileLength;
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+         
     }
 }
